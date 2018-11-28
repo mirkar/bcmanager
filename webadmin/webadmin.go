1ef1387ba34f03b765f93f777e4ec6d8e713d30e
dm0434 <dm0434@att.com>
2018-11-28 15:23:21 -0800

    added request header values printout endpoint

--- webadmin/webadmin.go
+++ webadmin/webadmin.go
@@ -24,6 +24,8 @@ func main() {
 
 	http.HandleFunc("/app/", dynamicContentHandler)
 
+	http.HandleFunc("/test/", testContentHandler)
+
 	handlerApiV1 = apiV1Handler{}
 	http.Handle("/api/v.1/", handlerApiV1)
 
@@ -38,6 +40,21 @@ func dynamicContentHandler(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprint(w, "Dynamic content handlerApiV1\n")
 	//handlerApiV1.ServeHTTP(w, r)
 }
+
+func testContentHandler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "%s %s %s \n", r.Method, r.URL, r.Proto)
+	//Iterate over all header fields
+	for k, v := range r.Header {
+		fmt.Fprintf(w, "Header field %q, Value %q\n", k, v)
+	}
+
+	fmt.Fprintf(w, "Host = %q\n", r.Host)
+	fmt.Fprintf(w, "RemoteAddr= %q\n", r.RemoteAddr)
+	//Get value for a specified token
+	fmt.Fprintf(w, "\n\nFinding value of \"Accept\" %q", r.Header["Accept"])
+
+}
+
 func (h apiV1Handler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	response := prometheus.Apache{
 		Env:         "production",
